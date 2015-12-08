294b197128c4fff2c404d9e690dcf4d375a325a3
Leo Li (Zhuhai) <Leo.Li.Zhuhai@software.dell.com>
2015-12-08 10:28:45 +0800

    do unit test

--- JsonParser.Test/JElementTest.cs
+++ JsonParser.Test/JElementTest.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace JsonParser.Test
 {
+    [TestClass]
     public class JElementTest
     {
+        [TestMethod]
+        public void TestJElement()
+        {
+            const string json = "{'UserName':'Jackson', UserAge:22, Tel:'13800138000', Email:'Jackson@mail.com', Cars:['Audi', 'BMW', 'Ferrari']}";
+            JElement ele = JElement.Parse(json);
+            ele.Should().NotBeNull();
+            "Jackson".Should().Equals(ele["UserName"].Value);
+            "22".Should().Equals(ele["UserAge"].Value);
+            "13800138000".Should().Equals(ele["Tel"].Value);
+            "Jackson@mail.com".Should().Equals(ele["Email"].Value);
+            "Audi".Should().Equals(ele["Cars"][0].Value);
+            "Ferrari".Should().Equals(ele["Cars"][2].Value);
+            ele["Cars"].Value.Length.Should().Equals(3);
+            ele["Cars"][100].Value.Should().BeNull();
+            ele["novalue"].Value.Should().BeNull();
+            ele["novalue"]["novalue"]["novalue"].Value.Should().BeNull();
+        }
     }
 }
